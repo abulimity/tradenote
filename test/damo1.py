364210bcd778fb733e9c8186430379330746811e
abulimity <18082895096@163.com>
2025-06-17 23:11:08 +0800

    6

--- test/damo1.py
+++ test/damo1.py
@@ -1,2 +1,9 @@
-from qlib.workflow import R
-recorder = R.get_recorder(recorder_id="a0d9142e8644405186b6741383dcc617")
+# python scripts/get_data.py qlib_data --target_dir ../qlib_data/cn_data --region cn
+
+import qlib
+# region in [REG_CN, REG_US]
+from qlib.constant import REG_CN
+provider_uri = "D:/project/tradenote/qlib_data/cn_data"  # target_dir
+qlib.init(provider_uri=provider_uri, region=REG_CN)
+
+# qrun benchmarks/LightGBM/workflow_config_lightgbm_Alpha1582.yaml
